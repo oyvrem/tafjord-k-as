21dabe838097903edc67d66c7608f550207f1567
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2019-06-08 14:55:21 +0200

    implemented BEM to Ansatt component

--- src/components/Ansatte/Ansatt.js
+++ src/components/Ansatte/Ansatt.js
@@ -6,23 +6,23 @@ import envelope from './../../static/icons/envelope-solid.svg';
 class Ansatt extends React.Component {
     render() {
         return(
-            <div className="card">
-                <div className="card-image">
+            <div className="employee-card">
+                <div className="employee-card__image">
                     <img src={this.props.ansattBilde} aria-hidden="true" />
                 </div>
-                <div className="card-body">
-                    <h3 className="card-title h4 mb-3">
+                <div className="employee-card__body">
+                    <h3 className="employee-card__body__title card-title h4 mb-3">
                         {this.props.ansattNavn}
                     </h3>
-                    <div className="lead">
+                    <div className="employee-card__body__text lead">
                         <p>
                             {this.props.ansattAvdeling}
                         </p>
                         <p>
-                            <img src={phone} className="responsive-icon" aria-hidden="true" /> <a href="#">{this.props.ansattTelefon}</a>
+                            <img src={phone} className="employee-card__body__text__icon responsive-icon" aria-hidden="true" /> <a href="#">{this.props.ansattTelefon}</a>
                         </p>
                         <p>
-                            <img src={envelope} className="responsive-icon" aria-hidden="true" /> <a href="#">{this.props.ansattEpost}</a>
+                            <img src={envelope} className="employee-card__body__text__icon" aria-hidden="true" /> <a href="#">{this.props.ansattEpost}</a>
                         </p>
                     </div>
                 </div>
