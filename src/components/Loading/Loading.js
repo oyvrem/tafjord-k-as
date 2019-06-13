132bdb26d2414864a067c452ca2ab0663ada53a7
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2019-06-13 15:01:51 +0200

    added text to spinner

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="[ d-flex align-items-center justify-content-center ]" style={{
+        <div className="[ d-flex align-items-center justify-content-center flex-column ]" style={{
             minHeight: '100vh',
             paddingBottom: '150px'
         }}>
-            <div className="spinner-border text-primary">
-                <span className="sr-only">Loading...</span>
-            </div>
+            <div className="spinner-border text-primary mb-3" aria-hidden="true"></div>
+            <strong>Loading...</strong>
         </div>
     )
 }
