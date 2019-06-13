5cca4881eaf2ab4281e265f97cb5dc87bd8f84cc
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2019-06-13 13:40:01 +0200

    added logo and found a methodic for extending bootstrap classes into component classes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Navigation.scss';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../../static/logo/logo_horizontal-color.svg';
 
 class Navigation extends React.Component {
     render() {
         return(
             <nav className="navbar navbar-expand navbar-light bg-light">
-                <Link to="/" className="navbar-brand">Tafjord K AS</Link>
+                <Link to="/" className="navbar-brand">
+                    <img src={logo} />
+                </Link>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
