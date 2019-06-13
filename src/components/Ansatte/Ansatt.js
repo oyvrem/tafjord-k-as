e8a75ab6c40983ead1d1bae198cacf0ac5813611
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2019-06-13 19:12:59 +0200

    adjusted some accessibility issues

--- src/components/Ansatte/Ansatt.js
+++ src/components/Ansatte/Ansatt.js
@@ -3,11 +3,11 @@ import './Ansatt.scss';
 import phone from './../../static/icons/phone-solid.svg';
 import envelope from './../../static/icons/envelope-solid.svg';
 
-const Ansatt = (props) => {
+const Ansatt = props => {
     return (
         <div className="employee-card">
             <div className="employee-card__image">
-                <img src={props.ansattBilde} aria-hidden="true" />
+                <img src={props.ansattBilde} aria-hidden="true" alt={`Et bilde av ${props.ansattBilde}`} />
             </div>
             <div className="employee-card__body">
                 <h3 className="employee-card__body__title card-title [ h4 mb-3 ]">
@@ -18,10 +18,10 @@ const Ansatt = (props) => {
                         {props.ansattAvdeling}
                     </p>
                     <p>
-                        <img src={phone} className="employee-card__body__text__icon" aria-hidden="true" /> <a href={`tel:${props.ansattTelefon}`} target="_blank">{props.ansattTelefon}</a>
+                        <img src={phone} className="employee-card__body__text__icon" aria-hidden="true" alt="Telefon" /> <a href={`tel:${props.ansattTelefon}`}>{props.ansattTelefon}</a>
                     </p>
                     <p>
-                        <img src={envelope} className="employee-card__body__text__icon" aria-hidden="true" /> <a href={`mailto:${props.ansattEpost}`} target="_blank">{props.ansattEpost}</a>
+                        <img src={envelope} className="employee-card__body__text__icon" aria-hidden="true" alt="Konvolutt" /> <a href={`mailto:${props.ansattEpost}`}>{props.ansattEpost}</a>
                     </p>
                 </div>
             </div>
