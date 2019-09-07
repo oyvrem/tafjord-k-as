eeb05f073f3cb37e0caf8992be182c15b90a5df5
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2019-09-07 03:09:40 +0200

    fixed scroll to functionality for viewing employees

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,6 +17,7 @@ class Home extends React.Component {
             ansatte: [],
             isLoading: true
         };
+        this.ansatteSeksjon = React.createRef();
     }
 
     componentWillMount() {
@@ -33,10 +34,10 @@ class Home extends React.Component {
             .catch(err => console.log(err));
     }
 
-    goToAnsatteSection = () => {
+    goToAnsatteSeksjon = () => {
         window.scrollTo(
             0,
-            document.getElementById('ansatte')
+            this.ansatteSeksjon.current.offsetTop
         );
     }
 
@@ -71,11 +72,11 @@ class Home extends React.Component {
                                     <p className="[ lead mb-4 ]">{this.state.forside.forside_header_tekst}</p>
                                     <p>
                                         <Link to="/tjenester" className="[ btn btn-lg btn-light mb-3 mr-3 ]">Se hva vi gjør</Link>
-                                        <button onClick={this.goToAnsatteSection} className="[ btn btn-lg btn-outline-light mb-3 ]">Prat med en av oss</button>
+                                        <button onClick={this.goToAnsatteSeksjon} className="[ btn btn-lg btn-outline-light mb-3 ]">Prat med en av oss</button>
                                     </p>
                                 </div>
                             </header>
-                            <section id="ansatte" className="[ container-fluid pt-5 pb-5 ]">
+                            <section ref={this.ansatteSeksjon} className="[ container-fluid pt-5 pb-5 ]">
                                 <div className="[ container ]">
                                     <h2 className="[ text-center mb-4 ]">Prat med en av oss</h2>
                                     <div className="[ row ]">
